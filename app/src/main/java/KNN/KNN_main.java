d981e7e3e4d6e66613cab7796ed3c78681de3124
Jinpei Chen <jinpei.chen@anu.edu.au>
2019-09-18 12:55:21 +1000

    initial commit

---
+++ app/src/main/java/KNN/KNN_main.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * K-Nearest Neighbor (KNN) method - Data training and testing
+ *
+ * Authors:         Jinpei Chen
+ *                  Yuzhao Li
+ *
+ * Created data:    10/09/2019
+ * Last modified:   13/09/2019
+ */
+public class KNN_main {
+    // created lists for storing training and testing datasets label and features.
+
+    private List<double[]> trainfeatures = new ArrayList<>();
+    private List<String> trainlabel = new ArrayList<>();
+
+    private List<double[]> testfeatures = new ArrayList<>();
+    private List<String> testlabel = new ArrayList<>();
+
+
+}
